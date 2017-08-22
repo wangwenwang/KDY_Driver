cf624a63cc001991a254a2a17013cb5c13ed8513
凯东源 <kaidongyuan@kaidongyuandeMacBook-Air.local>
2017-08-22 08:51:44 +0800

    LM

---
+++ newDriver/Controller/TableViewCell/PushOrderTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  PushOrderTableViewCell.swift
+//  newDriver
+//
+//  Created by 凯东源 on 16/12/15.
+//  Copyright © 2016年 凯东源. All rights reserved.
+//
+
+import UIKit
+
+class PushOrderTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
